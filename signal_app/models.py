eaf0e3c3d686f089c80117c6bc66ca234d88720f
daniil.sobolev <daniil.sobolev@lanit-tercom.com>
2021-07-05 17:31:34 +0300

    feat: add ability to save data to db

--- signal_app/models.py
+++ signal_app/models.py
@@ -18,12 +18,14 @@ class Detail(models.Model):
 
 
 class StandardProduct(models.Model):
+    number = models.CharField(max_length=50, help_text='Децимальный номер', null=True)
     name = models.CharField(max_length=500, help_text='Наименование')
     entry_number = models.ForeignKey(Assembly, on_delete=models.CASCADE)
     count_number = models.CharField(max_length=200, help_text='Количество')
 
 
 class OtherProduct(models.Model):
+    number = models.CharField(max_length=50, help_text='Децимальный номер', null=True)
     name = models.CharField(max_length=500, help_text='Наименование')
     entry_number = models.ForeignKey(Assembly, on_delete=models.CASCADE)
     count_number = models.CharField(max_length=200, help_text='Количество')
--- signal_app/views.py
+++ signal_app/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render, HttpResponse
 from .models import Assembly, Detail, StandardProduct, OtherProduct
+import json
+import enum
+
+
+class ObjectDoesNotExist(Exception):
+    pass
+
+
+class TypeEnum(enum.Enum):
+    ASSEMBLY = 0
+    DETAIL = 1
+    STANDARD_PRODUCTS = 2
+    OTHER_PRODUCTS = 3
+
+    __mapping__ = {
+        'ASSEMBLY': 'Cборка',
+        'DETAIL': 'Деталь',
+        'STANDARD_PRODUCTS': 'Стандартные изделия',
+        'OTHER_PRODUCTS': 'Прочие изделия',
+    }
 
 
 def index(request):
@@ -7,17 +27,41 @@ def index(request):
 
 
 def show_tree(request):
-    assemblies = Assembly.objects.all()
-    details = Detail.objects.all()
-    standard_products = StandardProduct.objects.all()
-    other_products = OtherProduct.objects.all()
-    print(assemblies)
-    print(details)
-    print(standard_products)
-    print(other_products)
+    assembly = Assembly.objects.all()
+    for i in assembly:
+        print(i.number)
     return render(request, 'base.html')
 
 
 def save_data(request):
-    print(request.body)
+    body = json.loads(request.body)
+    type_ = TypeEnum(int(body['type'])).name
+    body.update({'type': type_.title()})
+
+    model = eval(body['type'])
+    # if model != TypeEnum(type)
+    if model != TypeEnum.ASSEMBLY.name:
+        try:
+            item_parent = Assembly.objects.filter(number=body['vhod']).get()
+        except Assembly.DoesNotExist:
+            raise ObjectDoesNotExist("Object doesn't exist.")
+
+        new_db_object = model.objects.create(
+            number=body['number'],
+            name=body['name'],
+            entry_number=item_parent.id,
+            count_number=3
+        )
+        new_db_object.save()
+
+    model = eval(body['type'])  # converts string type to Python Class object
+    new_db_object = model.objects.create(
+        number=body['number'],
+        name=body['name'],
+        entry_number=body['vhod']
+    )
+    new_db_object.save()
+
+    return_new_object = model.objects.all().last()
+    print(return_new_object)
     return HttpResponse(request, {'Hello': 'world'})
