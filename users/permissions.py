3e2b3f36434a241639f56ce86327be6f6ee5baca
daniil.sobolev <daniil.sobolev@lanit-tercom.com>
2021-07-07 12:08:24 +0300

    feat: add custom permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,4 +1,12 @@
+from django.contrib.auth.models import User
+
 from .exceptions import UserDoesntHaveEnoughPermissions
+from .enums import UserTypeEnum
+
+
+class ProjectPermissions:
+    PERM_PROJECT_READ = 'PERM_PROJECT_READ'
+    PERM_PROJECT_UPDATE = 'PERM_PROJECT_UPDATE'
 
 
 def project_permissions_required(permissions: list):
@@ -8,11 +16,27 @@ def project_permissions_required(permissions: list):
     def real_decorator(function):
         def wrapper(*args, **kwargs):
             current_user = kwargs['request'].user
-            for perm in permissions:
-                if current_user.has_perm(perm):
-                    result = function(*args, **kwargs)
-                    return result
-                else:
-                    raise UserDoesntHaveEnoughPermissions("You don't have enough permissions.")
+            current_user_permissions = get_current_user_permissions(current_user)
+            if any(req_perm.startswith(user_perm) for user_perm in
+                   current_user_permissions for req_perm in permissions):
+                result = function(*args, **kwargs)
+            else:
+                raise UserDoesntHaveEnoughPermissions("You don't have enough permissions.")
+            return result
         return wrapper
     return real_decorator
+
+
+def get_current_user_permissions(user: User):
+    permissions = []
+
+    if user.user_type in [UserTypeEnum.LEAD_DESIGNER.value,
+                          UserTypeEnum.LEAD_TECHNOLOGIST.value,
+                          UserTypeEnum.SOFTWARE_ENGINEER.value]:
+        permissions.extend([ProjectPermissions.PERM_PROJECT_READ,
+                            ProjectPermissions.PERM_PROJECT_UPDATE])
+
+    else:
+        permissions.extend([ProjectPermissions.PERM_PROJECT_READ])
+
+    return permissions
